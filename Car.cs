d5b8a87a8cd0e3a42a1d27e6d740cedc097ef503
bodwood <bodiewood@hotmail.com>
2022-12-02 15:38:41 -0800

    initial files with public variables to access globally

---
+++ Car.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+
+public class Car
+{
+  public string MakeModel;
+  public int Price;
+  public int Miles;
+}
+
+public class Program
+{
+  public static void Main(){
+    
+  }
+}
