d1c4fcdff2e26df77daf622c99c03d54580b9e74
bodwood <bodiewood@hotmail.com>
2022-12-02 15:58:40 -0800

    add car instances and give values to these objects. List is created from cars

--- Car.cs
+++ Car.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 
-public class Car
+public class Car                  //constructor class
 {
   public string MakeModel;
   public int Price;
@@ -11,6 +11,40 @@ public class Car
 public class Program
 {
   public static void Main(){
-    
+    Car volkswagen = new Car();  //make new instance of car
+    volkswagen.MakeModel = "1974 Volkswagen Thing";
+    volkswagen.Price = 1100;
+    volkswagen.Miles = 368792;
+
+    Car yugo = new Car();  //make new instance of car
+    yugo.MakeModel = "1980 Yugo Koral";
+    yugo.Price = 700;
+    yugo.Miles = 56000;
+
+    Car ford = new Car();  //make new instance of car
+    ford.MakeModel = "1988 Ford Country Squire";
+    ford.Price = 1400;
+    ford.Miles = 239001;
+
+    Car amc = new Car();  //make new instance of car
+    amc.MakeModel = "1976 AMC Pacer";
+    amc.Price = 400;
+    amc.Miles = 198000;
+
+    List<Car> CarList = new List<Car>();
+    CarList.Add(volkswagen);
+    CarList.Add(yugo);
+    CarList.Add(ford);
+    CarList.Add(amc);
+
+    foreach(Car i in CarList){
+      Console.WriteLine(i.MakeModel + ", " + i.Price + ", " + i.Miles);
+    }
   }
+
+  // public showCars(e){
+  //   foreach(Car i in e){
+  //     Console.WriteLine(i);
+  //   }
+  // }
 }
