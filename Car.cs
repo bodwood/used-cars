964107bd8610c1be4b8db5cc285fd10ce2b5e9c8
bodwood <bodiewood@hotmail.com>
2022-12-03 14:13:43 -0800

    add method to check if a cars price is above the wanted max price

--- Car.cs
+++ Car.cs
@@ -1,28 +1,33 @@
 using System;
 using System.Collections.Generic;
 
-public class Car                  
+public class Car
 {
   public string MakeModel;
   public int Price;
   public int Miles;
 
 
-public Car(string makeModel, int price, int miles){
-  MakeModel = makeModel;
-  Price = price;
-  Miles = miles;
+  public Car(string makeModel, int price, int miles)
+  {
+    MakeModel = makeModel;
+    Price = price;
+    Miles = miles;
+  }
+
+  public bool WorthBuying(int maxPrice)
+  {
+    return (Price <= macPrice);
+  }
 }
 
 public class Program
 {
-  public static void Main(){
+  public static void Main()
+  {
     Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);  //make new instance of car
-
     Car yugo = new Car("1980 Yugo Koral", 700, 56000);  //make new instance of car
-
     Car ford = new Car("1988 Ford Country Squire", 1400, 239001);  //make new instance of car
-
     Car amc = new Car("1976 AMC Pacer", 400, 198000);  //make new instance of car
 
     List<Car> CarList = new List<Car>();
@@ -31,7 +36,8 @@ public class Program
     CarList.Add(ford);
     CarList.Add(amc);
 
-    foreach(Car i in CarList){
+    foreach (Car i in CarList)
+    {
       Console.WriteLine(i.MakeModel + ", " + i.Price + ", " + i.Miles);
     }
   }
@@ -41,5 +47,5 @@ public class Program
   //     Console.WriteLine(i);
   //   }
   // }
-}
+
 }
